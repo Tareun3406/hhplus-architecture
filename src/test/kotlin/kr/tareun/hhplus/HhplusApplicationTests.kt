2067da5eefb361fa0dad99c4f1e219c0d3d43ae5
Tareun3406 <tareun3406@gmail.com>
2024-12-22 21:09:49 +0900

    create Spring project

---
+++ src/test/kotlin/kr/tareun/hhplus/HhplusApplicationTests.kt
@@ -0,0 +1,13 @@
+package kr.tareun.hhplus
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class HhplusApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
